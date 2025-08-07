82a266db5a59336589f842cc3530b18132e122dc
Elvina <218727726+elvinaangga@users.noreply.github.com>
2025-08-07 23:49:54 +0900

    second commit

---
+++ src/main/java/web/dao/CarDaoImpl.java
@@ -0,0 +1,22 @@
+package web.dao;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+import web.models.Car;
+
+@Repository
+public class CarDaoImpl implements CarDao {
+    private final List<Car> cars = Arrays.asList(
+            new Car("Toyota", "Camry", 2022),
+            new Car("Honda", "Accord", 2021),
+            new Car("Ford", "Mustang", 2023),
+            new Car("Tesla", "Model 3", 2023),
+            new Car("BMW", "X5", 2022)
+    );
+
+    @Override
+    public List<Car> getAllCars() {
+        return cars;
+    }
+}
